367d43b25429a86ba73b2ad58284719bff9100f4
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-04-17 23:06:12 +0700

    refactor flip handler

--- src/features/board/cardRow/card/Card.js
+++ src/features/board/cardRow/card/Card.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { flipCard } from "../../boardSlice";
 import { useDispatch } from "react-redux";
-const logoLink = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
+const cardLogo = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
 
 export const Card = ({ content, card, id }) => {
     const dispatch = useDispatch();
     //if visible is false, we will set the cardText as a codecademy image logo
-    let cardText = <img src={logoLink} className="logo-placeholder" alt="Card option"></img>
+    let cardText = <img src={cardLogo} className="logo-placeholder" alt="Card option"></img>
 
     //if the visible property is true, then show the content of the card
     if (card.visible) {
-        cardText=content;
+        cardText = content;
     }
     //set normal style to make the className into card resting. If later it matched or not matched after flip, we can re assign cardStyle value to adjust the css style
-    //onClickHandler will flip the card
-    const onClickHandler = id => {
+    //click will fire different action depends on visible card
+    let click = () => {
+        flipHandler(id);
+    }
+    //flipHandler will run the flipCard 
+    let flipHandler = id => {
         dispatch(flipCard(id))
     }
     const cardStyle = 'resting';
-        return (
-            <button 
-                className={`card ${cardStyle}`}
-                onClick={() =>  onClickHandler(id)}
-                >
-                {cardText}
-            </button>
-        )
+    return (
+        <button
+            className={`card ${cardStyle}`}
+            onClick={click}
+        >
+            {cardText}
+        </button>
+    )
 }
